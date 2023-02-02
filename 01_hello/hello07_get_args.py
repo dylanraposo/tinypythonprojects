555c41a0b3e421f9e8cca7a4121e119d3ea30965
dylanraposo <drap0909@yahoo.com>
2023-02-02 10:58:41 -0500

    upload changes to git

--- 01_hello/hello07_get_args.py
+++ 01_hello/hello07_get_args.py
@@ -1,19 +1,20 @@
-# #!/usr/bin/env python3
-# # Purpose: Say hello
+#!/usr/bin/env python3
+# Purpose: Say hello
 
-# import argparse
+import argparse
 
-# def get_args():
-#     parser = argparse.ArgumentParser(description='Say hello')
-#     parser.add_argument('-n', '--name', metavar='name',
-#                         default='World', help='Name to greet')
-#     return parser.parse_args()
 
-# def main():
-#     args = get_args()
-#     print('Hello, ' + args.name + '!')
+def get_args():
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
+    return parser.parse_args()
 
-# if __name__ == '__main__':
-#     main()
 
-print __name__;
+def main():
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
+if __name__ == '__main__':
+    main()
