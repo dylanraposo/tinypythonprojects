460a7700a6e3731db9a5aaa3ce2767af9ec8615b
dylanraposo <drap0909@yahoo.com>
2023-02-03 12:32:24 -0500

    update

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,16 +1,33 @@
+#!/usr/bin/env python3
+
+
 """
-Author: Dylan Raposo
-Date: 2 / 3 / 2023
-Purpose: The purpose of this program is to greet a user by saying hellp followed by their name. In this case, Dylan Raposo.
+Author:  Dylan Raposo
+Date:    2 / 3 / 2023
+Purpose: Say hello
+
 """
 
 import argparse as ap
 
 
+# --------------------------------------------------
+def get_args():
+    """Get the command-line arguments"""
+
+    parser = ap.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', default='World', help='Name to greet')
+    return parser.parse_args()
+
+
+# --------------------------------------------------
 def main():
+    """Make a jazz noise here"""
 
-    # Get command line arguments
+    args = get_args()
+    print('Hello, ' + args.name + '!')
 
-    parser = ap.ArgumentParser(
-        description='Hello Mini Project',
-        formatter_class=ap.ArgumentDefaultsHelpFormatter)
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
